8933560adf830a68be4a30aeca21ef727943126d
Gurmeet Singh <gurmeetsf@gmail.com>
2017-05-25 17:18:41 -0700

    added pg logic

--- server/routes/public.ts
+++ server/routes/public.ts
@@ -3,28 +3,26 @@ import { Request, Response, Router } from "express";
 import { DATABASEURL } from '../config';
 
 var pg = require('pg')
-  , connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/hsdemo1'
+  , connectionString = process.env.DATABASE_URL || 'postgres://postgres:admin@localhost:5432/hsdemo1'
   , client
   , query;
 
 const publicRouter: Router = Router();
 
 publicRouter.get("/simple", (request: Request, response: Response) => {
-  var email = 'ffffffffff';
+  var email = '';
 
   client = new pg.Client(connectionString);
   client.connect();
   query = client.query('SELECT * FROM hscontacts');
 
   query.on('row', function (result) {
-    console.log(result);
-    console.log(result.email);
-    email = result.email;
+       email = result.email;
 
   });
   query.on('end', function () { client.end(); });
   response.json({
-    text: "Hello Angular 2 Test"+ email ,
+    text: "Hello Angular 2  ha ha Test"+ email ,
     title: "Greetings.",
   });
 });
