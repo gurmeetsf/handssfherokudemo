4452e28d37b1331594b5bef71073d8f81cafd5fa
Gurmeet Singh <gurmeetsf@gmail.com>
2017-05-25 17:47:20 -0700

    test

--- server/routes/public.ts
+++ server/routes/public.ts
@@ -3,26 +3,43 @@ import { Request, Response, Router } from "express";
 import { DATABASEURL } from '../config';
 
 var pg = require('pg')
-  , connectionString = process.env.DATABASE_URL || 'postgres://postgres:admin@localhost:5432/hsdemo1'
+  , connString = process.env.DATABASE_URL || 'postgres://postgres:admin@localhost:5432/hsdemo1'
   , client
   , query;
+
+ 
 
 const publicRouter: Router = Router();
 
 publicRouter.get("/simple", (request: Request, response: Response) => {
-  var email = '';
+  // var email = '';
 
-  client = new pg.Client(connectionString);
-  client.connect();
-  query = client.query('SELECT * FROM hscontacts');
+  // client = new pg.Client(connectionString);
+  // client.connect();
+  // query = client.query('SELECT * FROM hscontacts');
 
-  query.on('row', function (result) {
-       email = result.email;
+  // query.on('row', function (result) {
+  //      email = result.email;
 
-  });
-  query.on('end', function () { client.end(); });
+  // });
+  // query.on('end', function () { client.end(); });
+
+
+  pg.connect(connString, function(err, client, done) {
+		if(err) response.send("Could not connect to DB: " + err);
+		client.query('SELECT * FROM hscontacts', function(err, result) {
+			done();
+			if(err) return response.send(err);
+      console.log(result.rows)
+      
+			response.send(result.rows);
+
+		});
+	});
+
+
   response.json({
-    text: "Hello Angular 2  ha ha Test"+ email ,
+    text: "Hello Angular 2  ha ha Test" ,
     title: "Greetings.",
   });
 });
